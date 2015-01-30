7d67045c478734264c0035db58183a70394f1c98
wilsaj <wilson.andrew.j+github@gmail.com>
2015-01-30 17:50:35 -0600

    pull census data in as geojson

--- map.js
+++ map.js
@@ -1,13 +1,24 @@
 'use strict';
 
-var map = L.map('map', {
-    scrollWheelZoom: false,
-    center: [30.33, -97.7],
-    zoom: 10
-});
+$(function () {
+  var map = L.map('map', {
+      scrollWheelZoom: false,
+      center: [30.33, -97.7],
+      zoom: 10
+  });
 
-var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',{
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-});
+  var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',{
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+  });
 
-map.addLayer(layer);
+  map.addLayer(layer);
+
+  $.getJSON('./data/census/pop_total.geojson', function (data, status, xhr) {
+    L.geoJson(data, {
+        style: function (feature) {
+            return {color: '#0000ff'};
+        }
+    }).addTo(map);
+  });
+
+});
