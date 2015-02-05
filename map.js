749fcc9cdefb81644f6e7a8a7f5d2d6dc7da0e6a
wilsaj <wilson.andrew.j+github@gmail.com>
2015-02-05 08:40:22 -0600

    add some explanation of whats a going on

--- map.js
+++ map.js
@@ -5,8 +5,17 @@ $(function () {
   mapboxgl.util.getJSON('https://www.mapbox.com/mapbox-gl-styles/styles/outdoors-v6.json', function (err, style) {
     if (err) throw err;
     var key = 'area_per_capita';
+    var featureName = 'pools';
 
-    $.getJSON('./data/processed/pools-per-capita.geojson', function (data, status, xhr) {
+    var messagePre= 'This is a map of ' + featureName + ' per capita\n' +
+                    'in Travis County, TX.\n\n' +
+                    'This data comes from the <a href="ftp://ftp.ci.austin.tx.us/GIS-Data/Regional/coa_gis.html">City of \n' +
+                    'Austin</a>. It is derived from LiDar \n' +
+                    'data collected in 2013.\n\n';
+
+    var filename = './data/processed/' + featureName + '-per-capita.geojson';
+
+    $.getJSON(filename, function (data, status, xhr) {
 
       var breaks = turf.jenks(data, key, 10);
       var colors = chroma.scale('Blues').domain([0,1], breaks.length - 1).colors();
@@ -66,13 +75,14 @@ $(function () {
 
               if (features.length > 0) {
                 var feature = features[0];
-                var str = "Area per capita (sq ft): " + round(feature.properties.area_per_capita * 10.7639, 2) + '\n' +
-                          "features per capita: " + round(feature.properties.features_per_capita, 2);
-                document.getElementById('features').innerHTML = str;
+                var str = "Number of " + featureName + " per capita:  " + round(feature.properties.features_per_capita, 2) + '\n' +
+                          "Total sq ft per capita:     " + round(feature.properties.area_per_capita * 10.7639, 2) + '\n';
+                document.getElementById('features').innerHTML = messagePre + str;
                 map.style.setClassList(['hilight' + feature.properties.featureIndex]);
               }
               else {
-                document.getElementById('features').innerHTML = "";
+                document.getElementById('features').innerHTML = messagePre + 'Hover over a census block \n' +
+                                                                'to see more.';
                 map.style.setClassList([]);
               }
           });
