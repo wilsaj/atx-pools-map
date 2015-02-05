2b6acb5880405716701f85fca936e9ddd1aad1a4
wilsaj <wilson.andrew.j+github@gmail.com>
2015-02-05 08:17:46 -0600

    display and features per capita in the features box

--- map.js
+++ map.js
@@ -4,38 +4,89 @@ $(function () {
   mapboxgl.accessToken = 'pk.eyJ1Ijoid2lsc2FqIiwiYSI6InNRVHhMcTQifQ.ufmSZHZfDC0qWQQ65hmwLg';
   mapboxgl.util.getJSON('https://www.mapbox.com/mapbox-gl-styles/styles/outdoors-v6.json', function (err, style) {
     if (err) throw err;
+    var key = 'area_per_capita';
 
-    style.layers.push({
-      "id": "censusBlocksFill",
-      "type": "fill",
-      "source": "censusBlocks",
-      "paint": {
-        "fill-opacity": .2,
-        "fill-color": "#888",
-      }
-    });
+    $.getJSON('./data/processed/pools-per-capita.geojson', function (data, status, xhr) {
 
-    style.layers.push({
-      "id": "censusBlocksLine",
-      "type": "line",
-      "source": "censusBlocks",
-      "paint": {
-        "line-color": "#888",
-        "line-width": 2
-      }
-    });
+      var breaks = turf.jenks(data, key, 10);
+      var colors = chroma.scale('Blues').domain([0,1], breaks.length - 1).colors();
 
+      colors.forEach(function (color, i) {
+        var filter = [
+            "all",
+            [">=", key, breaks[i]],
+            ["<", key, breaks[i+1]]
+          ];
 
-    $.getJSON('./data/census/pop_total.geojson', function (data, status, xhr) {
+        if (i == colors.length - 1) {
+          filter[2][0] = "<=";
+        }
+
+        style.layers.push({
+          "id": "censusBlocksFill"+i,
+          "type": "fill",
+          "source": "censusBlocks",
+          "interactive": true,
+          "filter": filter,
+          "paint": {
+            "fill-opacity": .8,
+            "fill-color": color,
+          }
+        });
+      });
+
+      data.features.forEach(function (feature, featureIndex) {
+        feature.properties.featureIndex = featureIndex;
+        var hilightStyle = {
+          "id": "censusBlockOutline" + featureIndex,
+          "type": "line",
+          "source": "censusBlocks",
+          "filter": ["==", "featureIndex", featureIndex],
+          "paint": {
+            "line-color": "#888",
+            "line-width": 0.5
+          }
+        };
+        var paintClass = "paint.hilight" + featureIndex;
+        hilightStyle[paintClass] = {
+          "line-color": "#FFE545",
+          "line-width": 2
+        };
+        style.layers.push(hilightStyle);
+      });
+
       var map = new mapboxgl.Map({
         container: 'map',
         style: style,
-        center: [30.5, -97.70],
-        zoom: 9 
+        center: [30.3, -97.70],
+        zoom: 9
+      }).on('hover', function(e) {
+          map.featuresAt(e.point, {radius: 5}, function(err, features) {
+              if (err) throw err;
+
+              if (features.length > 0) {
+                var feature = features[0];
+                var str = "Area per capita (sq ft): " + round(feature.properties.area_per_capita * 10.7639, 2) + '\n' +
+                          "features per capita: " + round(feature.properties.features_per_capita, 2);
+                document.getElementById('features').innerHTML = str;
+                map.style.setClassList(['hilight' + feature.properties.featureIndex]);
+              }
+              else {
+                document.getElementById('features').innerHTML = "";
+                map.style.setClassList([]);
+              }
+          });
       });
+
       var censusBlocks = new mapboxgl.GeoJSONSource({data: data});
       map.addSource('censusBlocks', censusBlocks);
     });
   });
-
 });
+
+
+function round(number, places) {
+  var d = Math.pow(10, places);
+  return parseFloat(Math.round(number * d) / d).toFixed(places);
+}
+
